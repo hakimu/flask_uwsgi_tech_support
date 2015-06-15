9dc2f9861d46ef61eb40b29becd03295322dccd8
Hakimu Jackson <hakimu@newrelic.com>
2015-06-15 12:38:30 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hellooooo World!!!"
+	return "Hello World!!!"
 
 if __name__ == '__main__':
   app.run()
 
-# NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program uwsgi --http :8080 -w app:app --single-interpreter --enable-threads
