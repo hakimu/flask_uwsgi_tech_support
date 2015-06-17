84500132d4a8c62d0338403be87ea04da4144b5e
Hakimu Jackson <hakimu@newrelic.com>
2015-06-16 21:10:12 -0700

    Added date to app and templates

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return "Hello World!!!"
+@app.route('/<name>')
+def hello_world(name=None):
+	now = datetime.datetime.now()
+	return render_template('hello.html',name=name, now=now)
 
 if __name__ == '__main__':
   app.run()
